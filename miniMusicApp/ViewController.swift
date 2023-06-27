ac273519c0067ffb1c09b4608ab69ce6dbd74a8d
FOREKS <berkay.kaya@foreks.com>
2023-06-27 18:18:40 +0300

    first setup

---
+++ miniMusicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  miniMusicApp
+//
+//  Created by FOREKS on 27.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
