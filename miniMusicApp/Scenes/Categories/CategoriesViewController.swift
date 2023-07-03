89f9f44a7536a0be50468a8717453ed952360124
FOREKS <berkay.kaya@foreks.com>
2023-07-03 21:56:06 +0300

    add categories screen

--- miniMusicApp/Scenes/Categories/CategoriesViewController.swift
+++ miniMusicApp/Scenes/Categories/CategoriesViewController.swift
@@ -6,11 +6,84 @@
 //
 
 import UIKit
+import TinyConstraints
+import UIComponents
 
 final class CategoriesViewController: BaseViewController<CategoriesViewModel> {
-
+    
+    private let symbolTitleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.text = "Categories"
+        return label
+    }()
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.itemSize = CGSize(width: (UIScreen.main.bounds.width / 2) - 10 , height:  (UIScreen.main.bounds.width / 2) - 10)
+        layout.minimumInteritemSpacing = 5
+        layout.minimumLineSpacing = 5
+        layout.sectionInset = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(CategoriesCell.self, forCellWithReuseIdentifier: CategoriesCell.identifier)
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .white
+        addSubViews()
+        viewModel.didLoad()
+        subscribeViewModel()
+    }
+}
+
+// MARK: - UILayout
+extension CategoriesViewController {
+    private func addSubViews() {
+        addPageTitle()
+        addCollectionView()
+    }
+    
+    private func addPageTitle() {
+        self.navigationController?.isNavigationBarHidden = true
+        self.view.addSubview(symbolTitleLabel)
+        symbolTitleLabel.centerXToSuperview()
+        symbolTitleLabel.topToSuperview(usingSafeArea: true).constant = 10
+    }
+    
+    private func addCollectionView() {
+        view.addSubview(collectionView)
+        collectionView.topToBottom(of: symbolTitleLabel).constant = 5
+        collectionView.edgesToSuperview(excluding: .top, usingSafeArea: true)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+}
+
+// MARK: - SubscribeViewModel
+extension CategoriesViewController {
+    private func subscribeViewModel() {
+        viewModel.reloadData = { [weak self] in
+            guard let self = self else { return }
+            self.collectionView.reloadData()
+        }
+    }
+}
+
+// MARK: - CollectionView Delegates
+extension CategoriesViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.numberOfItems
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CategoriesCell.identifier, for: indexPath) as? CategoriesCell {
+            cell.set(viewModel: viewModel.cellForItemAt(indexPath: indexPath))
+            return cell
+        } else {
+            return UICollectionViewCell()
+        }
     }
 }
--- miniMusicApp/Scenes/Categories/CategoriesViewModel.swift
+++ miniMusicApp/Scenes/Categories/CategoriesViewModel.swift
@@ -6,17 +6,57 @@
 //
 
 import Foundation
+import DataProvider
+import UIComponents
 
-
-protocol CategoriesDataSource {}
-
-protocol CategoriesEventSource {
+protocol CategoriesDataSource {
+    var numberOfItems: Int { get }
+    func cellForItemAt(indexPath: IndexPath) -> CategoriesCellProtocol
 }
 
-protocol CategoriesProtocol: CategoriesDataSource, CategoriesEventSource {}
+protocol CategoriesEventSource {
+    var reloadData: VoidClosure? { get set }
+}
+
+protocol CategoriesProtocol: CategoriesDataSource, CategoriesEventSource {
+    func didLoad()
+}
 
 final class CategoriesViewModel: BaseViewModel, CategoriesProtocol {
-
- 
+    
+    // Privates
+    private var cellItems: [CategoriesCellProtocol] = []
+    
+    // EventSource
+    var reloadData: VoidClosure?
+    
+    // DataSource
+    var musicRepository: MusicRepositoryProtocol
+    var numberOfItems: Int {
+       return cellItems.count
+    }
+    
+    func cellForItemAt(indexPath: IndexPath) -> CategoriesCellProtocol {
+      return cellItems[indexPath.row]
+    }
+    
+    init(musicRepository: MusicRepositoryProtocol) {
+        self.musicRepository = musicRepository
+    }
+    
+    func didLoad() {
+        musicRepository.getCategories { result in
+            switch result {
+            case .success(let response):
+                guard let data = response.data else { return }
+                self.cellItems = data.map({ category in
+                    return CategoriesCellModel(categoryImageView: category.pictureMedium, categoryName: category.name)
+                })
+                self.reloadData?()
+            case .failure(let err):
+                print(err.localizedDescription)
+            }
+        }
+    }
 
 }
